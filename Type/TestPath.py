f9396f9e23ead87f79a59abf6b81a190944f59c2
striver6 <18434366689@163.com>
2020-05-01 23:03:29 +0800

    second commit

--- Type/TestPath.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-sample_data = pd.read_csv('/home/data/SampleData-5g.csv')
-print(sample_data)
--- Type/dmeo.py
+++
@@ -1,4 +0,0 @@
-# import torch
-#
-# print("naiqa",torch.load(r"D:\Camp\train_lib\data.pt"))
-print(55*400+3300)
